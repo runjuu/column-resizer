243276f1a7d86097a0fc89b505c6ade16584ed36
Runjuu <hi@runjuu.com>
2023-11-11 20:31:18 -0800

    dev(integration): add `react-cra5`

---
+++ integrations/react-cra5/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container, Section, Bar } from '@column-resizer/react';
+
+export const App = () => {
+  return (
+    <Container style={{ height: '100px', background: '#80808080' }}>
+      <Section minSize={50} />
+      <Bar size={10} style={{ background: 'currentColor', cursor: 'col-resize' }} />
+      <Section minSize={100} />
+    </Container>
+  );
+};
+
+export default App;
